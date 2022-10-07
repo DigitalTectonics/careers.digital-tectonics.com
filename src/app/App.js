72fc0b8121b130132bf638ff497ab766949abb0d
AndreaHabibb <andrea.habib@digital-tectonics.com>
2022-10-07 18:40:41 -0400

    feat: (DWT-6) added appbar and drawer

--- src/app/App.js
+++ src/app/App.js
@@ -1,22 +1,18 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Landing from "../pages/landing/Landing";
+import Appbar from "../components/Appbar";
+import DrawerComp from "../components/DrawerComp";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <DrawerComp />
+      <Appbar />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
